c7f4aa89b70ba0713819d67292c19cdebf6c28ca
Edson Saul Avila Ortiz <3685020900101@ingenieria.usac.edu.gt>
2021-08-18 17:29:12 -0600

    inicial del front

---
+++ front/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {NavBar} from './components/navbar/navbar'
+
+function App() {
+  return (
+    <div className="App" style={{backgroundColor:"#34495E"}}>
+      <NavBar></NavBar>
+    </div>
+  );
+}
+
+export default App;
