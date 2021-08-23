b267328d98ba482aad430d37932bb0e3defc1f00
Edson Saul Avila Ortiz <3685020900101@ingenieria.usac.edu.gt>
2021-08-22 18:15:27 -0600

    back conectado con front y cors

---
+++ back/app.py
@@ -0,0 +1,25 @@
+import flask
+from flask import request
+from flask_cors import CORS
+
+app = flask.Flask(__name__)
+CORS(app)
+#app.config["DEBUG"] = True
+
+ 
+@app.route('/', methods=['GET'])
+def home():
+    #print(content['code'])
+    response = flask.jsonify({"mensaje":"gerardo puto"})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+    
+@app.route('/', methods=['POST'])
+def compilar():
+    content = request.get_json()
+    #contenido = content.code
+    #print(content['code'])
+    response = flask.jsonify({"mensaje":content['code']})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+#app.run()
