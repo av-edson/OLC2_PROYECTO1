365912525721ae5937d404084af553c5c8f58c78
Edson Saul Avila Ortiz <3685020900101@ingenieria.usac.edu.gt>
2021-08-22 18:13:49 -0600

    front conectado con back

--- front/src/components/navbar/editor.js
+++ front/src/components/navbar/editor.js
@@ -1,50 +1,57 @@
 import React from "react";
-import CodeMirror from '@uiw/react-codemirror';
-import 'codemirror/keymap/sublime';
-import 'codemirror/theme/monokai.css';
+import { UnControlled as CodeMirror } from 'react-codemirror2';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/theme/material.css';
+import 'codemirror/lib/codemirror.css';
 import './nav.css';
 
 export class Editor extends React.Component{
     state={
-        code:'const a = \'Gerardo Hueco\';',
-        console:''
+        value:'',
     }
 
+    onChange = (editor, data, value) => {
+      this.setState({
+        code:'',
+        console:'',
+        stado:false,
+      });
+    };
     render(){
         return(
             <div className='todo'>
                 <h2 style={{backgroundColor:"#27AE60"}}>JOLC Editor</h2>
-                <div className='editor'>
-                  <h5>Input</h5>
+                <div className="editor">
                   <CodeMirror
                     value={this.state.code}
-                  lazyLoadMode={true}
-                  options={{
-                    theme: 'monokai',
-                    tabSize: 2,
-                    keyMap: 'sublime',
-                    mode: 'js',
+                    options={{
+                      mode: 'javascript',
+                      theme: 'material',
+                      lineNumbers: true
+                    }}
+                    onChange={(editor, data, value) => {
+                      this.setState({code:value})
                     }}
                   />
                 </div>
-                <div className='editor'>
-                  <h5>Input</h5>
+                <div className="editor">
                   <CodeMirror
                     value={this.state.console}
-                    lazyLoadMode={false}
                     options={{
-                      theme: 'monokai',
-                      tabSize: 2,
-                      keyMap: 'sublime',
-                      mode: 'js',
+                      mode: 'javascript',
+                      theme: 'material',
+                      lineNumbers: true
+                    }}
+                    onChange={(editor, data, value) => {
+                      this.setState({console:value})
                     }}
                   />
                 </div>
                    <br></br> 
-                <div class="d-grid gap-2">
-                  <button class="btn btn-outline-success btn-lg " type="button">Compilar
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-window-dock" viewBox="0 0 16 16">
-                      <path fill-rule="evenodd" d="M15 5H1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V5zm0-1H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v1zm1-1a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3z"/>
+                <div className="d-grid gap-2">
+                  <button className="btn btn-outline-success btn-lg " type="button" onClick={()=>this.compilar()}>Compilar
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-window-dock" viewBox="0 0 16 16">
+                      <path fillRule="evenodd" d="M15 5H1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V5zm0-1H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v1zm1-1a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3z"/>
                       <path d="M3 11.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm4 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm4 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"/>
                     </svg>
                   </button>
@@ -53,4 +60,19 @@ export class Editor extends React.Component{
             </div>
         );
     }
+
+    compilar(){
+      this.setState({estado:true})
+      //console.log(jsonEnviar)
+      fetch('https://apinuevajeje.herokuapp.com/',{
+                method:'POST',
+                headers: {"Content-Type":"application/json"},
+                body:JSON.stringify({"code":this.state.code})
+              }).then(async response =>{
+                    const json = await response.json() 
+                    //console.log(json)
+                    this.setState({console:"es: "+json.mensaje,estado:false})
+                  })
+    }
+    
 }
--- front/src/components/navbar/reportes.js
+++ front/src/components/navbar/reportes.js
@@ -15,18 +15,18 @@ export class Resports extends React.Component{
                 <h2 style={{backgroundColor:"#27AE60"}}>Zona de Reportes</h2>
                 <h3 style={{backgroundColor:"#27AE60"}}>Actual: {this.state.tipoReporte}</h3>
                 <br></br>
-                <div class="btn-group btn-group-lg" role="group" aria-label="Basic radio toggle button group">
-                  <input type="button" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off"
+                <div className="btn-group btn-group-lg" role="group" aria-label="Basic radio toggle button group">
+                  <input type="button" className="btn-check" name="btnradio" id="btnradio1" autoComplete="off"
                   onClick={()=>this.cambio(1)}></input>
-                  <label class="btn btn-outline-info" for="btnradio1">Tabla de Errores</label>
+                  <label className="btn btn-outline-info" htmlFor="btnradio1">Tabla de Errores</label>
 
-                  <input type="button" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off"
+                  <input type="button" className="btn-check" name="btnradio" id="btnradio2" autoComplete="off"
                   onClick={()=>this.cambio(2)}></input>
-                  <label class="btn btn-outline-info" for="btnradio2">Tabla de Simbolos</label>
+                  <label className="btn btn-outline-info" htmlFor="btnradio2">Tabla de Simbolos</label>
 
-                  <input type="button" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off"
+                  <input type="button" className="btn-check" name="btnradio" id="btnradio3" autoComplete="off"
                   onClick={()=>this.cambio(3)}></input>
-                  <label class="btn btn-outline-info" for="btnradio3">AST</label>
+                  <label className="btn btn-outline-info" htmlFor="btnradio3">AST</label>
                 </div> 
                 <br></br>
                 <br></br>
@@ -39,10 +39,10 @@ export class Resports extends React.Component{
                 {this.state.noReporte===3 && 
                     <div>
                         <h2>Gerardo hijo de puta</h2>
-                        <div class="card" style={{width: "18rem"}}>
-                          <img src="https://scontent.fgua3-2.fna.fbcdn.net/v/t1.18169-9/1385193_523307391084915_997128073_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=ba80b0&_nc_ohc=ZQ9IUO6vbw4AX8kF7jd&_nc_ht=scontent.fgua3-2.fna&oh=5d7b547aba81e2f97eaf9f667a1ff0ce&oe=61449C0E" class="card-img-top" alt=""></img>
-                          <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
+                        <div className="card" style={{width: "18rem"}}>
+                          <img src="https://scontent.fgua3-2.fna.fbcdn.net/v/t1.18169-9/1385193_523307391084915_997128073_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=ba80b0&_nc_ohc=ZQ9IUO6vbw4AX8kF7jd&_nc_ht=scontent.fgua3-2.fna&oh=5d7b547aba81e2f97eaf9f667a1ff0ce&oe=61449C0E" className="card-img-top" alt=""></img>
+                          <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
                           </div>
                         </div>
                     </div>
